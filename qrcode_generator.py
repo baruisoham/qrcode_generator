daffc3654d2d2955100e71fff520bbb19c892573
baruisoham <109133618+baruisoham@users.noreply.github.com>
2023-10-03 12:18:56 +0530

    Update Size functionality
    Updated the code to now ask the user for an image size they want.

--- qrcode_generator.py
+++ qrcode_generator.py
@@ -25,7 +25,6 @@ if __name__ == "__main__":
     text_input = input("Enter the text you want to encode in the QR code: ")
     location_input = input("Enter the location where you want to save the QR code: ")
     name_input = input("Enter the name for the QR code file (without extension): ")
-    #size_input = int(input("Enter the size of the QR code (1 to 40, with 1 being 21x21): "))
-    size_input = int(4)
-
+    size_input = int(input("Enter the size of the QR code (1 to 20, with 1 being 21x21): "))
+    
     generate_qr_code(text_input, location_input, name_input, size_input)
